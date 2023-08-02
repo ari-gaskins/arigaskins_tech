4c63f9c94fa08dd57ae5fa4bded8767beda4be37
Ari Gaskins <arigaskins.tech@gmail.com>
2023-08-02 12:04:52 -0400

    setup pages with default HTML, make pages accessible through url

--- arigaskins_tech/portfolio/urls.py
+++ arigaskins_tech/portfolio/urls.py
@@ -4,4 +4,7 @@ from . import views
 # set urls 
 urlpatterns = [
     path('', views.homepage),
+    path('work-history/', views.work_history),
+    path('certifications/', views.certifications),
+    path('alma-maters/', views.alma_maters),
 ]
